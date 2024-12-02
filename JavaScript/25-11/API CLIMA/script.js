894da13945ac5ad9cd3d435692757a211b7f8fcc
Caio <caio_s_moura@estudante.sesisenai.org.br>
2024-12-02 16:01:09 -0300

    new update

--- JavaScript/25-11/API CLIMA/script.js
+++ JavaScript/25-11/API CLIMA/script.js
@@ -1,1 +1,40 @@
-const apiKey = "dbe0045db4814b42b08193022242511"
+document
+   .getElementById("climaForm")
+   .addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      const cidade = document.getElementById("cidade").value;
+      const apiKey = "dbe0045db4814b42b08193022242511";
+
+      const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cidade}&lang=pt`;
+
+      try {
+         const response = await fetch(url);
+
+         if (!response.ok) {
+            throw new Error("Erro ao buscar o clima");
+         }
+
+         const data = await response.json();
+
+         if (data.erro) {
+            alert("Cidade não Encontrada");
+            return;
+         }
+
+         document.getElementById(
+            "temperatura"
+         ).textContent = `Temperatura: ${data.current.temp_c}°C`;
+         document.getElementById(
+            "sensacao"
+         ).textContent = `Sensação: ${data.current.feelslike_c}°C`;
+         document.getElementById(
+            "umidade"
+         ).textContent = `Umidade: ${data.current.humidity}%`;
+         document.getElementById(
+            "vento"
+         ).textContent = `Vento: ${data.current.wind_kph}Km/h`;
+      } catch (error) {
+         alert("Ocorreu um erro:", error.message);
+      }
+   });
